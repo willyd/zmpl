6551b19ff52186369a761f2e68f6f3a268ca74ce
Guillaume Dumont <dumont.guillaume@gmail.com>
2016-06-15 22:13:41 -0400

    More work on getting matplotlib and zmq working together

--- start_server.py
+++ start_server.py
@@ -4,7 +4,71 @@ import zmq
 import matplotlib
 matplotlib.use('QT4Agg')
 from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
 
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        #
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Expanding,
+                                   QtGui.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class MyStaticMplCanvas(MyMplCanvas):
+    """Simple canvas with a sine plot."""
+
+    def compute_initial_figure(self):
+        t = np.arange(0.0, 3.0, 0.01)
+        s = np.sin(2*np.pi*t)
+        self.axes.plot(t, s)
+
+
+class MyDynamicMplCanvas(MyMplCanvas):
+    """A canvas that updates itself every second with a new plot."""
+
+    def __init__(self, *args, **kwargs):
+        MyMplCanvas.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
+
+    def compute_initial_figure(self):
+        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
+
+    def update_figure(self):
+        # Build a list of 4 random integers between 0 and 10 (both inclusive)
+        l = [random.randint(0, 10) for i in range(4)]
+
+        self.axes.plot([0, 1, 2, 3], l, 'r')
+        self.draw()
+
+class MyFigure(QtGui.QDialog):
+
+    def __init__(self, parent):
+        QtGui.QDialog.__init__(self, parent)
+        self.vbox = QtGui.QVBoxLayout(self)
+        self.sc = MyStaticMplCanvas(self, width=5, height=4, dpi=100)
+        self.vbox.addWidget(self.sc)
+        
 
 class ZeroMQ_Listener(QtCore.QObject):
 
@@ -41,6 +105,8 @@ class ZeroMQ_Window(QtGui.QMainWindow):
         
         self.setCentralWidget(frame)
 
+        self.figs = []
+
         self.thread = QtCore.QThread()
         self.zeromq_listener = ZeroMQ_Listener()
         self.zeromq_listener.moveToThread(self.thread)
@@ -49,8 +115,13 @@ class ZeroMQ_Window(QtGui.QMainWindow):
         self.zeromq_listener.message.connect(self.signal_received)
         
         QtCore.QTimer.singleShot(0, self.thread.start)
+
+        
     
     def signal_received(self, message):
+        fig = MyFigure(self)
+        self.figs.append(fig)
+        fig.show()
         self.text_edit.append("%s\n"% message)
 
     def closeEvent(self, event):
